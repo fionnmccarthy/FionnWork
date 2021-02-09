7265a590f3c5345ede9375e7ae28bf2337e2dc39
Fionn McCarthy <G00301126@gmit.ie>
2021-02-09 22:09:55 +0000

    Week03 Lab Extra Work

---
+++ Week02/lab2.3.extra.py
@@ -0,0 +1,9 @@
+# extra.py
+# Lab 2.1  Extra Part
+# 
+# Author: Fionn McCarthy
+
+#message = str('I have eaten') + int()
+numberofburritos = 99
+print("I have eaten {} burritos".format(numberofburritos))
+
