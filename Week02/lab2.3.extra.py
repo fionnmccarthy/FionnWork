0e1afbf3a134a76860bb10e47a0ad3a386276d8d
Fionn McCarthy <G00301126@gmit.ie>
2021-03-01 21:59:21 +0000

    extra lab work revision

--- Week02/lab2.3.extra.py
+++ Week02/lab2.3.extra.py
@@ -1,9 +1,12 @@
 # extra.py
 # Lab 2.1  Extra Part
-# 
+# Extra work
 # Author: Fionn McCarthy
 
-#message = str('I have eaten') + int()
+#Enter variable as number of 
 numberofburritos = 99
 print("I have eaten {} burritos".format(numberofburritos))
+
+#Eggs can be used as is a string and 100 is a number therefore cannot
+
 
